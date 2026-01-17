37be1489d09837925eb8e6419f37aac956b4cf4f
justchecking716-ai <justchecking716@gmail.com>
2026-01-17 21:21:42 +0530

    Update data.js

--- data.js
+++ data.js
@@ -2,7 +2,35 @@ const results = [
   {
     enrollment: "2213338555",
     name: "Shivam Kumar",
+    father: "Manoj Kumar",
     course: "Diploma in Electronics Engineering",
-    result: "PASS"
+    semester: "5th",
+    result: "PASS",
+    subjects: [
+      {
+        code: "DPEE501",
+        name: "Estimation and Specification",
+        internal: 12,
+        external: 75
+      },
+      {
+        code: "DPEE502",
+        name: "Switchgear and Protection",
+        internal: 17,
+        external: 74
+      },
+      {
+        code: "DPEE503",
+        name: "Embedded System",
+        internal: 10,
+        external: 68
+      },
+      {
+        code: "DPEE504",
+        name: "Electrical Workshop",
+        internal: 18,
+        external: 49
+      }
+    ]
   }
 ];
